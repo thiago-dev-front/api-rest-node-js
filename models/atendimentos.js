38d89028dcd575d4c7d25dcdbf0f7fea4460fbe9
tg-developer <tg-developer@gmail.com>
2021-04-18 19:55:45 -0300

    Tratamento de Erros

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,16 +1,43 @@
+const moment = require('moment');
 const conexao = require('../infraestrutura/conexao');
 
 class Atendimento {
-  adiciona(atendimento) {
-    const sql = 'INSERT INTO Atendimentos SET ?';
+  adiciona(atendimento, res) {
+    const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+    const data = moment(atendimento.data, 'DD/MM/YYYY').format(
+      'YYYY-MM-DD HH:MM:SS',
+    );
+    const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+    const ClienteEhValido = atendimento.cliente.length >= 5;
+    const validacoes = [
+      {
+        nome: 'data',
+        valido: dataEhValida,
+        mensagem: 'Data deve ser maior ou igual a data atual',
+      },
+      {
+        nome: 'cliente',
+        valido: ClienteEhValido,
+        mensagem: 'Cliente deve ter pelo menos 5 caracteres',
+      },
+    ];
 
-    conexao.query(sql, atendimento, (erro, resultados) => {
-      if (erro) {
-        console.log(erro);
-      } else {
-        console.log(resultados);
-      }
-    });
+    const erros = validacoes.filter((campo) => !campo.valido);
+    const existemErros = erros.length;
+    if (existemErros) {
+      res.status(400).json(erros);
+    } else {
+      const atendimentoDatado = { ...atendimento, dataCriacao, data };
+      const sql = 'INSERT INTO Atendimentos SET ?';
+
+      conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+        if (erro) {
+          res.status(400).json(erro);
+        } else {
+          res.status(201).json(resultados);
+        }
+      });
+    }
   }
 }
 
